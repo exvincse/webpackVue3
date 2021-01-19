b5e7b09ebcb8be3574facffe6ea092c681e10174
ruiyangliu <ruiyang.liu@aplus-digital.com>
2021-01-19 13:41:56 +0800

    調整babel

---
+++ test/f01/index.test.js
@@ -0,0 +1,11 @@
+// import { mount } from '@vue/test-utils'
+
+import aaa from '../../src/main/component/f01/index.vue'
+
+describe('Component', () => {
+  test('is a Vue instance', () => {
+    // const wrapper = mount(Component)
+    // expect(wrapper.isVueInstance()).toBeTruthy()
+    console.log(123)
+  })
+})
