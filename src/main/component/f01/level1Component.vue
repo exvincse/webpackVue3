1864eb94368b0e84c636b2a93630ddc34b64af27
ruiyangliu <ruiyang.liu@aplus-digital.com>
2021-03-17 18:36:49 +0800

    tmp

--- src/main/component/f01/level1Component.vue
+++ src/main/component/f01/level1Component.vue
@@ -1,26 +1,30 @@
 <!-- props -->
 <template>
     <div>
-        <level2-component v-bind="$attrs" v-on="$listeners" :level2value="level2value"></level2-component>
+        <button class="addData" @click="addData()">getData</button>
+        <button class="removeData" @click="removeData()">removeData</button>
+        <!-- <level2-component v-bind="$attrs" v-on="$lis。level2value="level2value"></level2-component> -->
     </div>
 </template>
 
 <script>
-import level2Component from './level2Component';
+// import level2Component from './level2Component';
 export default {
+    props: {
+        ary: {
+            type: Array
+        }
+    },
     components: {
-        level2Component
+        // level2Component
     },
     data() {
-        return {
-            level2value: 'level2value'
-        };
+        return {};
     },
     computed: {},
     watch: {},
     beforeCreate() { },
     created() {
-        console.log(this.count)
     },
     beforeMount() { },
     mounted() { },
@@ -28,6 +32,15 @@ export default {
     updated() { },
     beforeDestroy() { },
     destroyed() { },
-    methods: {}
+    methods: {
+        addData() {
+            
+        },
+        removeData() {
+            let ary = [...this.ary];
+            ary.splice(0,1);
+            this.$emit('updateData', ary)
+        }
+    }
 };
 </script>
--- test/f01/index.test.js
+++ test/f01/index.test.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex';
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import f01 from '../../src/main/store/modules/f01';
 import index from '../../src/main/component/f01/index.vue';
+import level1 from '../../src/main/component/f01/level1Component.vue';
 import flushPromises from 'flush-promises';
 const localVue = createLocalVue()
 localVue.use(Vuex)
@@ -31,34 +32,105 @@ describe('AsyncButton', () => {
   let store;
   let state;
   let actions;
-  beforeEach(() => {
-    state = {
-      jsonUsersData: []
-    }
-    actions = {
-      jsonUsers: jest.fn(() => promise([1,3]))
-    }
+  let ary = [{
+    "id": 1,
+    "name": "Fintone",
+    "email": "bfulton0@fda.gov",
+    "key": "12wehRiuD9oQZD1x7QmEX55CoLyhd5teiS"
+  }, {
+    "id": 2,
+    "name": "Zaam-Dox",
+    "email": "fbassam1@mapy.cz",
+    "key": "19q2qt4kdetu8c54mbYj1YNcbnrvbxFJ6L"
+  }, {
+    "id": 3,
+    "name": "Stim",
+    "email": "elay2@alibaba.com",
+    "key": "1KwfSyYbprsqzwZqjd2AV22V9AxRp8JVoc"
+  }, {
+    "id": 4,
+    "name": "Lotlux",
+    "email": "wisselee3@yandex.ru",
+    "key": "1JotVs65ENDCiVZUh15HncVNkurpY4Cw5D"
+  }, {
+    "id": 5,
+    "name": "Voyatouch",
+    "email": "ggercken4@scientificamerican.com",
+    "key": "1415EqAL7QJHBCynrgSTeWX5GrTqUv12p6"
+  }, {
+    "id": 6,
+    "name": "Bitchip",
+    "email": "dmerring5@artisteer.com",
+    "key": "1Kihs4Pj7pEVFPoHZ6BgRCwxFLgimFdw5F"
+  }, {
+    "id": 7,
+    "name": "Keylex",
+    "email": "pemtage6@microsoft.com",
+    "key": "1LVnZxyTUtampsb2ytxm1m56YmAa9wo4xq"
+  }, {
+    "id": 8,
+    "name": "Tres-Zap",
+    "email": "cboggers7@about.me",
+    "key": "1Asp46Fi4MJ7iQMNN7ggRzxNi5odhj2oJB"
+  }, {
+    "id": 9,
+    "name": "Kanlam",
+    "email": "yfranceschielli8@weibo.com",
+    "key": "17FVnx3Xo6yjQjR8Hycz3ZN2iZBiKhSmc2"
+  }, {
+    "id": 10,
+    "name": "Flexidy",
+    "email": "bscallon9@nymag.com",
+    "key": "13BWVVqRRCsj7dPT8n8XDaWN8tCusWuF3y"
+  }]
+  // beforeEach(() => {
+  //   state = {
+  //     jsonUsersData: [],
+  
+  //   }
+  //   actions = {
+  //     jsonUsers: jest.fn(() => promise())
+  //   }
     
-    store = new Vuex.Store({
-      modules: {
-        f01: {
-          state: state,
-          mutations: f01.mutations,
-          actions: actions,
-          namespaced: true
-        }
-      }
-    })
-  })
+  //   store = new Vuex.Store({
+  //     modules: {
+  //       f01: {
+  //         state: state,
+  //         mutations: f01.mutations,
+  //         actions: actions,
+  //         namespaced: true
+  //       }
+  //     }
+  //   })
+  // })
 
-  it('測試vuex資料', async () => {
-    let wrapper = shallowMount(index, { store, localVue })
-    await wrapper.find('button').trigger('click');
-    await flushPromises();
-    store.commit('f01/setJsonUsers', [1,3]);
-    wrapper.vm.$nextTick(() => {
-      expect(store.state.f01.jsonUsersData.length).toBe(2);
-      // console.log(wrapper.get('.test').text())
-    })
+  // it('測試vuex資料', async () => {
+  //   let wrapper = shallowMount(index, { store, localVue })
+  //   await wrapper.find('.addCount').trigger('click');
+  //   await flushPromises();
+  //   store.commit('f01/setJsonUsers', ary);
+  //   await wrapper.find('.button').trigger('click');
+  //   state.jsonUsersData.splice(1,1);
+  //   wrapper.vm.$nextTick(() => {
+  //     expect(store.state.f01.jsonUsersData.length).toBe(2);
+  //   })
+  // })
+
+  it('測試', async () => {
+    let index1 = shallowMount(index, {
+      localVue,
+      propsData: {
+        startData: '2021/03/17'
+      }
+    });
+    let level11 = shallowMount(level1, {
+      localVue
+    });
+    await level11.setProps({ary: ary});
+    await level11.find('.removeData').trigger('click');
+    expect(level11.props('ary').length).toBe(10);
+    ary.splice(0,1);
+    await index1.vm.$emit('updateData', ary);
+    expect(index1.emitted().updateData.length).toBe(1);
   })
 })
