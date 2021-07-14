4f429bdcbcb009ff0ad6e38f06f46b1ff7cae221
ruiyangliu <ruiyang.liu@aplus-digital.com>
2021-07-14 11:23:11 +0800

    add qrcode

--- src/main/component/f01/index.vue
+++ src/main/component/f01/index.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
-        <div v-for="(item, index) in ary" :key="index">
+        <!-- <div v-for="(item, index) in ary" :key="index">
             <span class="test">{{ item }}</span>
         </div>
         <button class="getData" @click="getData()">getData</button>
         <button class="removeData" @click="removeData()">removeData</button>
-        <level1-component :ary="ary" @updateData="updateData"></level1-component>
+        <level1-component :ary="ary" @updateData="updateData"></level1-component> -->
+        <qrcode-stream :camera="camera" :torch="torch" @init="onInit"></qrcode-stream>
     </div>
 </template>
 <script>
@@ -13,28 +14,22 @@ import { mapState } from 'vuex';
 import level1Component from './level1Component';
 export default {
     components: {
-        level1Component
+        level1Component,
     },
-    data() {
+    data () {
         return {
-            // ary: []
+            camera: 'auto',
+            torch: true
         }
     },
-    computed: {
-        ...mapState({
-            ary: state => JSON.parse(JSON.stringify(state.f01.jsonUsersData))
-        })
-    },
+
     methods: {
-        async getData() {
-            let res = await this.$store.dispatch('f01/jsonUsers');
-            // this.ary = res;
-        },
-        removeData() {
-            this.ary.splice(0,1);
-        },
-        updateData(item) {
-            this.ary = item;
+        async onInit (promise) {
+            try {
+                const { capabilities } = await promise;
+            } catch (error) {
+            
+            }
         }
     }
 }
--- src/main/main.js
+++ src/main/main.js
@@ -3,10 +3,12 @@ import store from "./store/index";
 import "./content/scss/all.scss";
 import App from './App.vue';
 import Vue from 'vue';
+import VueQrcodeReader from "vue-qrcode-reader";
 
+Vue.use(VueQrcodeReader);
 
 let app = new Vue({
     router,
     store,
     render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
